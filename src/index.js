3edf15976b29670f32a7085f3ab243d0d0794f12
RajuKoushik <g.rajukoushik@gmail.com>
2019-02-22 07:27:48 -0700

    Updated an extra prop to accommodate 2 players
    Signed-off-by: RajuKoushik <g.rajukoushik@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
+
 
 class Board extends React.Component {
 
@@ -24,13 +18,17 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsnext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsnext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsnext: !this.state.xIsnext,
+        });
     }
 
 
@@ -44,7 +42,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsnext ? 'X' : 'O');
 
         return (
             <div>
